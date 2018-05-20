ba52bc678eba94b23ec6e68b76d597a78927ebf2
shin4299 <shin4299@gmail.com>
2018-05-20 11:53:22 +0900

    Update esp-gesture.js

--- esp-gesture.js
+++ esp-gesture.js
@@ -9,7 +9,6 @@ module.exports = (homebridge) => {
   Characteristic = homebridge.hap.Characteristic;
 
   homebridge.registerAccessory('homebridge-esp-gesture', 'ESPGESTURE', ESPGESPlugin);
-  homebridge.registerAccessory('homebridge-esp-gesture', 'ESPGESTUREb', ESPGESPluginb);
 };
 
 class ESPGESPlugin
@@ -17,6 +16,7 @@ class ESPGESPlugin
   constructor(log, config) {
     this.log = log;
     this.name = config.name;
+    this.num = config.number || 1;
     this.listen_port = config.listen_port || 8265;
 	  
 	this.informationService = new Service.AccessoryInformation();
@@ -26,7 +26,7 @@ class ESPGESPlugin
       .setCharacteristic(Characteristic.Model, "ESPEasyGesture")
       .setCharacteristic(Characteristic.SerialNumber, this.device);
 	  
-	this.switch1Service = new Service.StatelessProgrammableSwitch(this.name + '1');
+	this.switch1Service = new Service.StatelessProgrammableSwitch(this.name + tihs.num);
 //	this.switch2Service = new Service.StatelessProgrammableSwitch(this.name + '2');
     
 
@@ -49,7 +49,7 @@ class ESPGESPlugin
       }
 
       const gesture = json.gesture;
-
+	if (this.num == '1') {
 	    	if (gesture == '1') {
         this.switch1Service
 	.getCharacteristic(Characteristic.ProgrammableSwitchEvent)
@@ -63,80 +63,8 @@ class ESPGESPlugin
 	.getCharacteristic(Characteristic.ProgrammableSwitchEvent)
      	.setValue(Characteristic.ProgrammableSwitchEvent.LONG_PRESS);
 		}
-	    
-	    /*else if (gesture == '4') {
-        this.switch1Service
-	.getCharacteristic(Characteristic.ProgrammableSwitchEvent.BUTTON_2)
-     	.setValue(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS);
-		} else if (gesture == '5') {
-        this.switch1Service
-	.getCharacteristic(Characteristic.ProgrammableSwitchEvent.BUTTON_2)
-     	.setValue(Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS);
-		} else if (gesture == '6') {
-        this.switch1Service
-	.getCharacteristic(Characteristic.ProgrammableSwitchEvent.BUTTON_2)
-     	.setValue(Characteristic.ProgrammableSwitchEvent.LONG_PRESS);
-		}*/
-		
-		
-
-	    
-    });
-
-    
-    this.server.bind(this.listen_port);
-
-  }
-
-  getServices() {
-	  
-	return [this.informationService, this.switch1Service];
-//	return [this.informationService, this.switch1Service, this.switch2Service];
-
-  }
-}
-
-
-
-class ESPGESPluginb
-{
-  constructor(log, config) {
-    this.log = log;
-    this.name = config.name;
-    this.listen_port = config.listen_port || 8265;
-	  
-	this.informationService = new Service.AccessoryInformation();
-
-    this.informationService
-      .setCharacteristic(Characteristic.Manufacturer, "ESP")
-      .setCharacteristic(Characteristic.Model, "ESPEasyGestureb")
-      .setCharacteristic(Characteristic.SerialNumber, this.device);
-	  
-	this.switch1Service = new Service.StatelessProgrammableSwitch(this.name + '2');
-//	this.switch2Service = new Service.StatelessProgrammableSwitch(this.name + '2');
-    
-
-    this.server = dgram.createSocket('udp4');
-    
-    this.server.on('error', (err) => {
-      console.log(`udp server error:\n${err.stack}`);
-      this.server.close();
-    });
-
-    this.server.on('message', (msg, rinfo) => {
-      console.log(`server received udp: ${msg} from ${rinfo.address}`);
-
-      let json;
-      try {
-          json = JSON.parse(msg);
-      } catch (e) {
-          console.log(`failed to decode JSON: ${e}`);
-          return;
-      }
-
-      const gesture = json.gesture;
-
-	    	if (gesture == '4') {
+	} else { 
+		if (gesture == '4') {
         this.switch1Service
 	.getCharacteristic(Characteristic.ProgrammableSwitchEvent)
      	.setValue(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS);
@@ -149,7 +77,7 @@ class ESPGESPluginb
 	.getCharacteristic(Characteristic.ProgrammableSwitchEvent)
      	.setValue(Characteristic.ProgrammableSwitchEvent.LONG_PRESS);
 		}
-	    
+	}
 	    /*else if (gesture == '4') {
         this.switch1Service
 	.getCharacteristic(Characteristic.ProgrammableSwitchEvent.BUTTON_2)
@@ -181,3 +109,4 @@ class ESPGESPluginb
 
   }
 }
+
